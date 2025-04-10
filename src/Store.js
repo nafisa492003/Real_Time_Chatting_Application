775c49584bc5e7b7d7e05682f88c3cce0ac490c1
nafisa492003 <afifayasmin2003@gmail.com>
2025-04-11 03:17:53 +0600

    fixing netlify UI issues and done

--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./Component/Slices/userSlice";
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./Store.js";
-import { DarkModeProvider } from '../src/Component/NavBar/DarkModeProvider.jsx';
-createRoot(document.getElementById('root')).render(
+import { DarkModeProvider } from "../src/Component/NavBar/DarkModeProvider.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./Store.js";
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
+    </PersistGate>
   </Provider>
-)
+);
