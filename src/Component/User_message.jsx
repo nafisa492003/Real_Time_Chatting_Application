ed7343c216fbdf1df6296c583400e3ec20dd12ef
nafisa492003 <afifayasmin2003@gmail.com>
2025-04-12 17:04:27 +0600

    solving error issue

--- src/Component/User_message.jsx
+++ src/Component/User_message.jsx
@@ -6,7 +6,6 @@ const User_message = ({ onUserSelect }) => {
   const db = getDatabase();
   const [userList, setUserList] = useState([]);
   const userData = useSelector((state) => state.user.user);
-  if (!userData) return null;
 //   users data
 useEffect(() => {
   const userRef = ref(db, "users/");
